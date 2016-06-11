869825ee0af5c72da513376bf1c3317d7b5828d2
Air <air@macbook-air-air>
2016-06-11 19:21:43 +0300

    Initial Commit

---
+++ game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  game
+//
+//  Created by Air on 11.06.16.
+//  Copyright © 2016 Air. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
